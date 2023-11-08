e20d4ee7eba600a00f843c9ad9a860c6befebac2
Jason Inouye <jasonjinouye@gmail.com>
2023-11-08 16:05:18 -0600

    TrackContext and useSaveTrack hook created

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,22 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default axios.create({
-    baseURL: 'https://7a01-2601-449-4500-cca0-8179-4afd-d21f-e74c.ngrok-free.app'
-})
+const instance =  axios.create({
+    baseURL: 'https://f4c8-2601-449-4500-cca0-7577-a1d-94a6-922c.ngrok-free.app'
+});
+
+instance.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (err) => {
+        return Promise.reject(err);
+    }
+)
+
+export default instance;
+
--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -2,7 +2,8 @@ import React, {useContext } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Input, Button } from 'react-native-elements';
 import Spacer from './Spacer';
-import { Context as LocationContext } from '../context/LocationContext'
+import { Context as LocationContext } from '../context/LocationContext';
+import useSaveTrack from '../hooks/useSaveTrack';
 
 const TrackForm = () => {
     const {
@@ -12,17 +13,26 @@ const TrackForm = () => {
         changeName 
     } = useContext(LocationContext);
 
-    console.log(locations.length);
+    const [saveTrack] = useSaveTrack();
 
     return(
         <>
             <Spacer>
                 <Input value={name} onChangeText={changeName} placeholder="Enter Name" />
             </Spacer>
-            {
-                recording ? <Button title='Stop' onPress={stopRecording} /> :
-                <Button title="Start Recording" onPress={startRecording}/>
-            }
+            <Spacer>
+                {
+                    recording ? <Button title='Stop' onPress={stopRecording} /> :
+                    <Button title="Start Recording" onPress={startRecording}/>
+                }
+            </Spacer>
+            <Spacer>
+                {
+                    !recording && locations.length ? (
+                        <Button title="Save Recording" onPress={saveTrack}/>
+                    ) : null
+                }
+            </Spacer>
         </>
     );
 };
